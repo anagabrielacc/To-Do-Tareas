5468de1d760ffdceeb503b7fe2e6d9fbe8112fac
anagabrielacc <soyanagabriela08@gmail.com>
2025-02-12 20:58:47 -0500

    Push Tareas

--- toDoTareas.js
+++ toDoTareas.js
@@ -17,15 +17,50 @@ const li=document.createElement('li');
 li.innerHTML=`<span>${textoTarea}</span>
                 <button class="borrar-btn" onClick="borrarTarea(this)">Eliminar </button>`;
 
+li.querySelector('span').addEventListener('click',function(){
+     this.parentElement.classList.toggle('completado');
+
+})
+                
 listadoTareas.appendChild(li);
 
 nuevaTarea.value='';
+guardarTareasEnLocalStorage();
 }
 
 function borrarTarea(elementoTarea){
 elementoTarea.parentElement.remove();
 
+}
+
+const inputNuevaTarea=document.getElementById('nuevaTarea');
+inputNuevaTarea.addEventListener('keypress',function(tecla){
+     if(tecla.key==='Enter'){agregarTarea()};
+     
+});
+
+function guardarTareasEnLocalStorage(){
+const tareas=[];
+document.querySelectorAll('li').forEach(tarea=>{
+tareas.push({
+     Nombre_Tarea:tarea.querySelector('span').innerText,
+     Estado_Tarea:tarea.classList.contains ('comletado') 
+})
+
+})
+localStorage.setItem('tareasGuardadas, tareas');
+
+
 
 }
+
+
+
+
+
+
+
+
+
 
    
